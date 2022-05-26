f09326c8b84d8813abfb43db5016f046ebe32548
Gan Hiranmartsuwan <72602371+sonrayy@users.noreply.github.com>
2022-05-27 03:10:58 +0700

    Remove sensitive token.

--- login.js
+++ login.js
@@ -12,7 +12,7 @@ function login() {
     const xhttp = new XMLHttpRequest();
     xhttp.open("POST", "https://restapi.tu.ac.th/api/v1/auth/Ad/verify2");
     xhttp.setRequestHeader("Content-Type", "application/json");
-    xhttp.setRequestHeader("Application-Key", "TU0261e56e18732dfbc1e37a3d1de235bc7274857639ea4534c3c6b55392bbc939a3f90b04f5b03905d31e344d32dd2b6b");
+    xhttp.setRequestHeader("Application-Key", "use your own acces token");
     xhttp.send(JSON.stringify({
       "UserName": username,
       "PassWord": password
